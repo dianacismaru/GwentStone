322e9e3167de47b42c7beefc30d6794eca8f5741
Diana Cismaru <cismaru_diana02@yahoo.com>
2022-11-11 02:40:54 +0200

    Add Helper class

---
+++ src/main/Helper.java
@@ -0,0 +1,69 @@
+package main;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import fileio.CardInput;
+
+public class Helper {
+    private Helper(){
+    }
+
+    /**
+     * Checks if an input card can be an environment card
+     * @param  card the card that will be checked
+     * @return      true if the card has an environment card name, false otherwise
+     */
+    public static boolean canBeEnvironmentCard(CardInput card) {
+        String name = card.getName();
+        return name.equals("Winterfell") || name.equals("Firestorm") || name.equals("Heart Hound");
+    }
+
+    /**
+     * Creates an ObjectNode from a card
+     * @param  card the card that will be parsed
+     * @return      the newly-created ObjectNode
+     */
+    public static ObjectNode createCardNode(Card card) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectNode cardNode = objectMapper.createObjectNode();
+
+        cardNode.put("mana", card.getMana());
+
+        if (!(card instanceof EnvironmentCard)) {
+            cardNode.put("attackDamage", card.getAttackDamage());
+            cardNode.put("health", card.getHealth());
+        }
+        cardNode.put("description", card.getDescription());
+
+        ArrayNode colorsArrayNode = cardNode.putArray("colors");
+        for (String color: card.getColors()) {
+            colorsArrayNode.add(color);
+        }
+        cardNode.put("name", card.getName());
+
+        return cardNode;
+    }
+
+    /**
+     * Creates an ObjectNode from a hero card
+     * @param  hero the hero card that will be parsed
+     * @return      the newly-created ObjectNode
+     */
+    public static ObjectNode createHeroNode(Card hero) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectNode heroNode = objectMapper.createObjectNode();
+
+        heroNode.put("mana", hero.getMana());
+        heroNode.put("description", hero.getDescription());
+
+        ArrayNode colorsArrayNode = heroNode.putArray("colors");
+        for (String color: hero.getColors()) {
+            colorsArrayNode.add(color);
+        }
+        heroNode.put("name", hero.getName());
+        heroNode.put("health", hero.getHealth());
+
+        return heroNode;
+    }
+}
--- src/main/Round.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-public class Round {
-    Player[] players;
-
-    public Round(GameSet gameSet) {
-        this.players = gameSet.players;
-    }
-}
